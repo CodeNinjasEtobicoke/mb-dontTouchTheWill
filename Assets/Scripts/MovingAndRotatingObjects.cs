1b563a40dc6343bde605d13be936973418b91abb
AstroBoy2013 <astrobrandon2013@gmail.com>
2025-02-19 17:46:08 -0500

    2 skibidi 3
    w willy wonka

---
+++ Assets/Scripts/MovingAndRotatingObjects.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class MovingAndRotatingObjects : MonoBehaviour
+{
+    [Header("Default Fortnite Skin And Movement Speed")]
+    public float moveSpeed = 10f;
+    [Header("Default Rotating Fortnite Skin And Speed")]
+    public float rotateSpeed = 50f;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(0, moveSpeed * Time.deltaTime, 0);
+        transform.Rotate * Time.deltaTime, 0);
+    }
+}
---
+++ Assets/Scripts/SpawnObjects.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnObjects : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [Header("spawn the cube for god sakes")]
+    public GameObject spawnCube;
+    [Header("Default fortnite skin but difficulty")]
+    public float difficulty = 40f;
+    float spawn;
+    // Update is called once per frame
+    void Update()
+    {
+        spawn += difficulty * Time.deltaTime;
+        while(spawn > 0)
+        {
+            spawn -= 1;
+            Vector3 v3Pos = transform.position + new Vector3(Random.value * 40f - 20f, 0, Random.value * 40f - 20f);
+            Quaternion qRotation = Quaternion.Euler(0, Random.value * 360f, Random.value * 30f);
+            GameObject createObject = Instantiate(spawnCube, v3Pos, qRotation);
+        }
+    }
+}
