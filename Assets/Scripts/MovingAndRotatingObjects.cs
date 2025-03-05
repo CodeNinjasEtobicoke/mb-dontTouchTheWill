ea46e6867fb62aee01a6417a259d3626eadc9636
Code Ninjas Etobicoke <adambro1031@proton.me>
2025-03-05 16:23:58 -0500

    Will told me to make a caption
    Will IS NOt Skibidi

--- Assets/Scripts/MovingAndRotatingObjects.cs
+++ Assets/Scripts/MovingAndRotatingObjects.cs
@@ -9,13 +9,33 @@ public class MovingAndRotatingObjects : MonoBehaviour
     public float moveSpeed = 10f;
     [Header("Default Rotating Fortnite Skin And Speed")]
     public float rotateSpeed = 50f;
+
+    Material material;
+
     // Start is called before the first frame update
-    
+    private void Start()
+    {
+        material = GetComponent<MeshRenderer>().material;
+    }
+
     // Update is called once per frame
     void Update()
     {
         transform.Translate(0, moveSpeed * Time.deltaTime, 0);
         transform.Rotate(Vector3.up * rotateSpeed * Time.deltaTime);
+
+        ChangeColor();
+    }
+
+    void ChangeColor()
+    {
+        float r = Random.Range(0f, 1f);
+        float g = Random.Range(1f, 0f);
+        float b = Random.Range(0f, 1f);
+
+        Color color = new Color(r, g, b);
+        material.color = color;
+
     }
 }
 
